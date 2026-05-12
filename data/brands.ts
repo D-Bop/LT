436da5ddd46d56dcfdce91aa045c724f658228eb
D-Bop <official.og.owolabi@gmail.com>
2026-05-12 10:15:26 +0100

    fixed some image display issues and updated products

--- data/brands.ts
+++ data/brands.ts
@@ -19,7 +19,7 @@ export const brands = [
     id: "audemars-piguet",
     name: "Audemars Piguet",
     tagline: "The Mark of Excellence",
-    image: "/brands/audemars-piguet.jpg",
+    image: "/brands/audemars-Piguet.jpg",
     products: [
       {
         id: "ap-1",
@@ -326,41 +326,523 @@ export const brands = [
       },
     ]
   },
+
+  //  Cartier
   {
     id: "cartier",
     name: "Cartier",
     tagline: "Never Imitate, Always Innovate",
     image: "/brands/Cartier.jpg",
   },
+
+  //  Poedagar
   {
     id: "poedagar",
     name: "Poedagar",
     tagline: "Sharp Wrists. Sharper Minds.",
-    image: "/brands/Poedagar.webp",
+    image: "/brands/Poedagar.jpeg",
+    products: [
+      {
+        id: "p-1",
+        name: "Poedagar Rhinemaster Chronograph",
+        description: "Blacked out Analogue Watch with Luxurious Stainless Steel Chain Strap and Red Accents",
+        price: "contact for price",
+        image: "/products/poedagar/p-1.jpeg"
+      },
+      {
+        id: "p-2",
+        name: "Poedagar Rhinemaster Chronograph",
+        description: "Blacked out Analogue Watch with Luxurious Stainless Steel Chain Strap and Yellow Accents",
+        price: "contact for price",
+        image: "/products/poedagar/p-2.jpeg"
+      },
+      {
+        id: "p-3",
+        name: "Poedagar Rhinemaster Chronograph",
+        description: "Blacked out Analogue Watch with Luxurious Stainless Steel Chain Strap and Gold Accents",
+        price: "contact for price",
+        image: "/products/poedagar/p-3.jpeg"
+      },
+      {
+        id: "p-4",
+        name: "Poedagar Trace Classic",
+        description: "Luxurious Stainless Italian Steel with Chain Strap, Silver Bezel and Black Dial",
+        price: "contact for price",
+        image: "/products/poedagar/p-4.jpeg"
+      },
+      {
+        id: "p-5",
+        name: "Poedagar Trace Classic",
+        description: "Luxurious Stainless Italian Steel with Chain Strap, Silver Bezel and Blue Dial",
+        price: "contact for price",
+        image: "/products/poedagar/p-3.jpeg"
+      },
+      {
+        id: "p-6",
+        name: "Poedagar Trace Classic - Gold Edition",
+        description: "Luxurious Stainless Italian Steel with Chain Strap, Gold Bezel and Gold Dial",
+        price: "contact for price",
+        image: "/products/poedagar/p-6.jpeg"
+      },
+      {
+        id: "p-7",
+        name: "Poedagar Corsair",
+        description: "Luxurious Stainless Italian Steel with Chain Strap, Silver Bezel and Black Dial",
+        price: "contact for price",
+        image: "/products/poedagar/p-7.jpeg"
+      },
+      {
+        id: "p-8",
+        name: "Poedagar Corsair",
+        description: "Luxurious Stainless Italian Steel with Chain Strap, Silver Bezel and Azurite Blue Dial",
+        price: "contact for price",
+        image: "/products/poedagar/p-8.jpeg"
+      },
+      {
+        id: "p-9",
+        name: "Poedagar Corsair",
+        description: "Luxurious Stainless Italian Steel with Chain Strap, Silver Bezel and White Dial",
+        price: "contact for price",
+        image: "/products/poedagar/p-9.jpeg"
+      },
+      {
+        id: "p-10",
+        name: "Poedagar Corsair - Provence Edition",
+        description: "Luxurious Stainless Italian Steel with Chain Strap, Rose Gold Bezel and Black Dial",
+        price: "contact for price",
+        image: "/products/poedagar/p-10.jpeg"
+      },
+      {
+        id: "p-11",
+        name: "Poedagar Corsair - Provence Edition",
+        description: "Luxurious Stainless Italian Steel with Chain Strap, Rose Gold Bezel and Azurite Blue Dial",
+        price: "contact for price",
+        image: "/products/poedagar/p-11.jpeg"
+      },
+      {
+        id: "p-12",
+        name: "Poedagar Intrepid Chronograph",
+        description: "Crystal Lens, Leather Strap, Black Dial and Bezel Watch",
+        price: "contact for price",
+        image: "/products/poedagar/p-12.jpeg"
+      },
+      {
+        id: "p-13",
+        name: "Poedagar Intrepid Chronograph",
+        description: "Crystal Lens, Leather Strap, Silver Bezel and Gold Dial Watch",
+        price: "contact for price",
+        image: "/products/poedagar/p-13.jpeg"
+      },
+      {
+        id: "p-14",
+        name: "POEDAGAR 857 Top Rectangle",
+        description: "Luxurious Stainless Steel, Dark Blue Mirror Dial, Quartz Movement.",
+        price: "contact for price",
+        image: "/products/poedagar/p-14.jpeg"
+      },
+      {
+        id: "p-15",
+        name: "POEDAGAR 857 Top Rectangle",
+        description: "Luxurious Stainless Steel, Silver Gold Mirror Dial, Quartz Movement.",
+        price: "contact for price",
+        image: "/products/poedagar/p-15.jpeg"
+      },
+      {
+        id: "p-16",
+        name: "POEDAGAR 857 Top Rectangle",
+        description: "Luxurious Stainless Steel, Black Gold Mirror Dial, Quartz Movement.",
+        price: "contact for price",
+        image: "/products/poedagar/p-16.jpeg"
+      },
+      {
+        id: "p-17",
+        name: "POEDAGAR 857 Top Rectangle",
+        description: "Luxurious Stainless Steel, Silver Black Mirror Dial, Quartz Movement.",
+        price: "contact for price",
+        image: "/products/poedagar/p-17.jpeg"
+      },
+      {
+        id: "p-18",
+        name: "POEDAGAR Low Rider - Black Edition",
+        description: "Luxurious Stainless Steel,  Black Bezel and Dial, Precision Quartz Movement.",
+        price: "contact for price",
+        image: "/products/poedagar/p-18.jpeg"
+      },
+      {
+        id: "p-19",
+        name: "POEDAGAR Low Rider",
+        description: "Luxurious Stainless Steel, Silver Bezel and Black Dial,Precision Quartz Movement.",
+        price: "contact for price",
+        image: "/products/poedagar/p-19.jpeg"
+      },
+      {
+        id: "p-20",
+        name: "POEDAGAR Low Rider",
+        description: "Luxurious Stainless Steel, Black Bezel and Dial with Gold Accents,Precision Quartz Movement.",
+        price: "contact for price",
+        image: "/products/poedagar/p-20.jpeg"
+      },
+    ]
   },
+
+  // Rolex
   {
     id: "rolex",
     name: "Rolex",
     tagline: "Every Rolex tells a story",
     image: "/brands/Rolex.jpg",
+    products: [
+      {
+        id: "r-1",
+        name: "Rolex Day-Date 40",
+        description: "Ref. 228235 featuring an Everose gold case and a unique Eisenkiesel dial.",
+        price: "Contact for price",
+        image: "/products/rolex/r-1.jpeg"
+      },
+      {
+        id: "r-2",
+        name: "Rolex Sky Dweller",
+        description: "distinct sunburst blue dial, a fluted bezel made of white gold, and a Jubilee bracelet.",
+        price: "Contact for price",
+        image: "/products/rolex/r-2.jpeg"
+      },
+      {
+        id: "r-3",
+        name: "Rolex Day-Date 40",
+        description: "crafted in 18 ct Everose gold with a slate ombré dial..",
+        price: "Contact for price",
+        image: "/products/rolex/r-3.jpeg"
+      },
+      {
+        id: "r-4",
+        name: "Rolex Datejust 36",
+        description: "Crafted in 18kt yellow gold with a champagne Roman numeral dial and a Jubilee bracelet.",
+        price: "Contact for price",
+        image: "/products/rolex/r-4.jpeg"
+      },
+      {
+        id: "r-5",
+        name: "Rolex Lady-Datejust 28",
+        description: "two-tone steel and rose gold design, featuring a white dial with diamond markers.",
+        price: "Contact for price",
+        image: "/products/rolex/r-5.jpeg"
+      },
+      {
+        id: "r-6",
+        name: "Rolex Lady-Datejust",
+        description: "Yellow Gold with a silver Roman numeral dial and a Jubilee bracelet.",
+        price: "Contact for price",
+        image: "/products/rolex/r-6.jpeg"
+      },
+      {
+        id: "r-7",
+        name: "Rolex Datejust 28",
+        description: "chocolate dial and diamond hour markers",
+        price: "Contact for price",
+        image: "/products/rolex/r-7.jpeg"
+      },
+      {
+        id: "r-8",
+        name: "Rolex Datejust 41",
+        description: "two-tone rose gold and stainless steel design with a slate Wimbledon dial and Jubilee bracelet.",
+        price: "Contact for price",
+        image: "/products/rolex/r-8.jpeg"
+      },
+      {
+        id: "r-9",
+        name: "Rolex Datejust 41mm",
+        description: " It is constructed from Oystersteel (stainless steel) and 18k white gold, The watch features a distinctive mint green sunray dial and a fluted white gold bezel.",
+        price: "Contact for price",
+        image: "/products/rolex/r-9.jpeg"
+      },
+      {
+        id: "r-10",
+        name: "Rolex Datejust",
+        description: "yellow gold finish with a grey dial and diamond hour markers.",
+        price: "Contact for price",
+        image: "/products/rolex/r-10.jpeg"
+      },
+      {
+        id: "r-11",
+        name: "Rolex Datejust 41",
+        description: "The watch features a Wimbledon dial with a rose gold and stainless steel bracelet.",
+        price: "Contact for price",
+        image: "/products/rolex/r-11.jpeg"
+      },
+      {
+        id: "r-13",
+        name: "Rolex Sky-Dweller 326934",
+        description: "stainless steel watch with a blue dial and Jubilee bracelet.",
+        price: "Contact for price",
+        image: "/products/rolex/r-13.jpeg"
+      },
+      {
+        id: "r-14",
+        name: "Rolex Datejust 41",
+        description: "Two Tone Everose Gold Wimbledon 126331.",
+        price: "Contact for price",
+        image: "/products/rolex/r-14.jpeg"
+      },
+      {
+        id: "r-15",
+        name: "Rolex Sky-Dweller 336934",
+        description: "featuring a 42mm case made of Oystersteel and 18kt white gold, distinctive fluted bezel, a mint green sunburst dial, and a second time zone display via an off-center 24-hour disc.",
+        price: "Contact for price",
+        image: "/products/rolex/r-15.jpeg"
+      },
+      {
+        id: "r-16",
+        name: "Rolex Day-Date 40",
+        description: "features an 18kt yellow gold case, bezel, and 'President' bracelet.",
+        price: "Contact for price",
+        image: "/products/rolex/r-16.jpeg"
+      },
+    ]
   },
+
+  // Tissot
   {
     id: "tissot",
     name: "Tissot",
     tagline: "Innovators by tradition",
     image: "/brands/Tissot.jpg",
+    products: [
+      {
+        id: "t-3",
+        name: "Tissot PRX",
+        description: "Luxurious Stainless Steel with Silver Bezel and White Dial",
+        price: "Contact for price",
+        image: "/products/tissot/t-3.jpeg"
+      },
+      {
+        id: "t-2",
+        name: "Tissot PRX Quartz watch",
+        description: "Luxurious Stainless Steel with Gold Bezel and Black Dial",
+        price: "Contact for price",
+        image: "/products/tissot/t-2.jpeg"
+      },
+      {
+        id: "t-4",
+        name: "Tissot PRX Quartz watch",
+        description: "Luxurious Stainless Steel with Gold Bezel and Gold Chrome Dial",
+        price: "Contact for price",
+        image: "/products/tissot/t-4.jpeg"
+      },
+      {
+        id: "t-5",
+        name: "Tissot PRX Quartz watch",
+        description: "Luxurious Stainless Steel with Silver Bezel and Azurite Blue Dial",
+        price: "Contact for price",
+        image: "/products/tissot/t-5.jpeg"
+      },
+      {
+        id: "t-6",
+        name: "Tissot PRX Quartz watch",
+        description: "featuring a green waffle-textured dial and matching green rubber strap.",
+        price: "Contact for price",
+        image: "/products/tissot/t-6.jpeg"
+      },
+      {
+        id: "t-7",
+        name: "Tissot PRX Quartz watch",
+        description: "Textured black dial and a blue rubber strap",
+        price: "Contact for price",
+        image: "/products/tissot/t-7.jpeg"
+      },
+      {
+        id: "t-8",
+        name: "Tissot PRX Quartz watch",
+        description: "Black forged carbon case and a black rubber strap.",
+        price: "Contact for price",
+        image: "/products/tissot/t-8.jpeg"
+      },
+      {
+        id: "t-9",
+        name: "Tissot PRX Powermatic 80",
+        description: "gold-colored dial, stainless steel case, and an orange rubber strap.",
+        price: "Contact for price",
+        image: "/products/tissot/t-9.jpeg"
+      },
+      {
+        id: "t-10",
+        name: "Tissot PRX Powermatic 80",
+        description: "featuring a black waffle dial, a gold-plated bezel, and a black rubber strap",
+        price: "Contact for price",
+        image: "/products/tissot/t-10.jpeg"
+      },
+    ]
   },
+
+  // Hislon 
   {
     id: "hislon",
     name: "Hislon",
     tagline: "Crafted with Accuracy and Class",
     image: "/brands/hislon-saatini-kaydet-2.jpg",
+    products: [
+      {
+        id: "h-1",
+        name: "Hislon 1928",
+        description: "featuring a silver dial and stainless steel band.",
+        price: "contact for price",
+        image: "/products/hislon/h-1.jpeg"
+      },
+      {
+        id: "h-2",
+        name: "Hislon 1928",
+        description: "two-tone stainless steel and gold strap with a green textured dial.",
+        price: "contact for price",
+        image: "/products/hislon/h-2.jpeg"
+      },
+      {
+        id: "h-3",
+        name: "Hislon 1928",
+        description: "luxury men's wristwatch in a two-tone silver and rose gold finish.",
+        price: "contact for price",
+        image: "/products/hislon/h-3.jpeg"
+      },
+      {
+        id: "h-4",
+        name: "Hislon Classic Collection ultra-flat quartz wristwatch",
+        description: "gold-tone stainless steel chain bracelet.",
+        price: "contact for price",
+        image: "/products/hislon/h-4.jpeg"
+      },
+      {
+        id: "h-5",
+        name: "Hislon 1928",
+        description: "Luxurious stainless steel men's wristwatches with a gray dial and sapphire crystal.",
+        price: "contact for price",
+        image: "/products/hislon/h-5.jpeg"
+      },
+      {
+        id: "h-6",
+        name: "Hislon 1928",
+        description: "Sapphire Ultra Slim men's wristwatch with a blue dial.",
+        price: "contact for price",
+        image: "/products/hislon/h-6.jpeg"
+      },
+      {
+        id: "h-7",
+        name: "Hislon 1928 Sapphire Ultra Slim",
+        description: "featuring a black leather strap with a crocodile pattern and a gold-toned clasp.",
+        price: "contact for price",
+        image: "/products/hislon/h-7.jpeg"
+      },
+      {
+        id: "h-8",
+        name: "Hislon 1928 Sapphire Ultra Slim",
+        description: "featuring a black dial and a leather strap",
+        price: "contact for price",
+        image: "/products/hislon/h-8.jpeg"
+      },
+      {
+        id: "h-9",
+        name: "Hislon CL127T-14SF",
+        description: "sapphire ultra-slim wristwatch with a black textured dial and a black leather strap.",
+        price: "contact for price",
+        image: "/products/hislon/h-9.jpeg"
+      },
+      {
+        id: "h-10",
+        name: "Hislon Ultra Slim watch",
+        description: "featuring a green striped dial and a matching green leather strap.",
+        price: "contact for price",
+        image: "/products/hislon/h-10.jpeg"
+      },
+      {
+        id: "h-11",
+        name: "Hislon Sapphire Ultra Slim",
+        description: "Men's Watch with a white dial, two-tone silver and rose gold case, and a black leather strap.",
+        price: "contact for price",
+        image: "/products/hislon/h-11.jpeg"
+      },
+      {
+        id: "h-12",
+        name: "Hislon Sapphire Ultra Slim",
+        description: "featuring a silver striped dial and a black leather strap.",
+        price: "contact for price",
+        image: "/products/hislon/h-12.jpeg"
+      },
+      {
+        id: "h-13",
+        name: "Hislon Sapphire Ultra Slim",
+        description: "featuring a silver-tone octagonal case, a white dial with horizontal lines, and a brown leather strap.",
+        price: "contact for price",
+        image: "/products/hislon/h-13.jpeg"
+      },
+      {
+        id: "h-14",
+        name: "Hislon Ultra Slim",
+        description: "featuring a two-tone octagonal bezel and a textured white dial.",
+        price: "contact for price",
+        image: "/products/hislon/h-14.jpeg"
+      },
+      {
+        id: "h-15",
+        name: "Hislon Ultra Slim CL127N Series",
+        description: "Japanese quartz movement, a sapphire crystal face, and is water-resistant up to 3 ATM, blue dial and Blue Silicone Rubber Strap",
+        price: "contact for price",
+        image: "/products/hislon/h-15.jpeg"
+      },
+      {
+        id: "h-16",
+        name: "Hislon Ultra Slim CL127N Series",
+        description: "Japanese quartz movement, a sapphire crystal face, and is water-resistant up to 3 ATM, Green dial and Green Silicone Rubber Strap",
+        price: "contact for price",
+        image: "/products/hislon/h-16.jpeg"
+      },
+    ]
   },
   {
     id: "valenzo",
     name: "Valenzo",
     tagline: "Time flies, But Shadows Always Prevail",
     image: "/brands/valenzo.jpeg",
+    products: [
+      {
+        id: "v-1",
+        name: "Engraved Arabic",
+        description: "Plastic Chain, Blacked out Bezel and Dial",
+        price: "contact for price",
+        image: "/products/valenzo/v-1.jpeg"
+      },
+      {
+        id: "v-2",
+        name: "Engraved Arabic",
+        description: "Plastic Chain, White Bezel and Dial",
+        price: "contact for price",
+        image: "/products/valenzo/v-2.jpeg"
+      },
+      {
+        id: "v-3",
+        name: "Arabic Continent",
+        description: "Plastic Chain, Black Bezel and Blue Dial",
+        price: "contact for price",
+        image: "/products/valenzo/v-3.jpeg"
+      },
+      {
+        id: "v-4",
+        name: "Arabic Continent",
+        description: "Plastic Chain, Black Bezel and Dial",
+        price: "contact for price",
+        image: "/products/valenzo/v-4.jpeg"
+      },
+      {
+        id: "v-5",
+        name: "Valenzo's Trident Arabic",
+        description: "Plastic Chain, Matte Black Bezel and Dial",
+        price: "contact for price",
+        image: "/products/valenzo/v-5.jpeg"
+      },
+      {
+        id: "v-6",
+        name: "Engraved Arabic",
+        description: "Plastic Chain, Matte Black Bezel and Dial",
+        price: "contact for price",
+        image: "/products/valenzo/v-6.jpeg"
+      },
+    ]
   },
 
 ];
